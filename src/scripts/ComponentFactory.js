4376f2803464e894b86a0cf1adf46351199f6da4
Audrey Langlois <azpmaggie@gmail.com>
2021-03-26 13:16:56 -0400

    Moitier de la modal de terminer, regler des problemes de header (probleme d'opacity dans la version desktop)

--- src/scripts/ComponentFactory.js
+++ src/scripts/ComponentFactory.js
@@ -1,8 +1,8 @@
 /** Importe les scripts necessaires pour ce ficher JS */
 import Scrolly from './components/Scrolly';
 import Carousel from './components/Carousel';
-import Modal from './components/Modal';
 import Header from './components/Header';
+import Modal from './components/Modal';
 
 //Code commence ici
 export default class ComponentFactory {
